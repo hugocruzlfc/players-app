4f06c6dddca91ba66e44e5047b7521bb97808050
hugocruzlfc <hugocruzlfc@gmail.com>
2024-01-22 13:14:02 +0100

    feat: adding game list section

--- src/components/Home/GameList.tsx
+++ src/components/Home/GameList.tsx
@@ -1,7 +1,38 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+import { GameListData } from "./../../shared";
 
 export interface GameListProps {}
 
 export const GameList: React.FC<GameListProps> = ({}) => {
-  return <div></div>;
+  // const [games, setGames] = useState();
+
+  // useEffect(() => {
+  //   setGames(GameListData);
+  // }, []);
+
+  return (
+    <div
+      className="grid grid-cols-3 sm:grid-cols-4
+    md:grid-cols-5 lg:grid-cols-7 mt-4"
+    >
+      {GameListData?.map((item) => (
+        <div
+          key={item.id}
+          // onClick={() => onGamePress(item.name)}
+          className="flex flex-col 
+            items-center cursor-pointer
+            "
+        >
+          <img
+            src={item.image}
+            width={45}
+            height={45}
+            className="hover:animate-bounce transition-all 
+                duration-150"
+          />
+          <h2 className="text-[14px] text-center">{item.name}</h2>
+        </div>
+      ))}
+    </div>
+  );
 };
